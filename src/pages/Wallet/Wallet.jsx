c03ea246f4c4647e9bae8c085560ea286a4a9ea7
Rabiul Islam <rabiul.dev@gmail.com>
2022-08-13 12:28:47 +0600

    wallet page responsive added

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -1,28 +1,24 @@
-import CommonHeader from '../../components/CommonHeader/CommonHeader'
-import ExploreBtn from '../../components/ExploreBtn/ExploreBtn'
-import walletData from '../../fakeDB/json/wallet.json'
-import WalletItems from './WalletItems'
+import CommonHeader from "../../components/CommonHeader/CommonHeader";
+import ExploreBtn from "../../components/ExploreBtn/ExploreBtn";
+import walletData from "../../fakeDB/json/wallet.json";
+import WalletItems from "./WalletItems";
 const Wallet = () => {
-    return (
+  return (
+    <div>
+      <CommonHeader />
+      <div className="container mx-auto py-[120px]">
+        <div className="py-[120px]">
+          <ul className="space-y-6 px-4">
+            {walletData.map((item) => (
+              <WalletItems key={item.id} item={item} />
+            ))}
+          </ul>
 
-        <div>
-            <CommonHeader />
-            <div className='container mx-auto py-[120px]'>
-
-                <div className='py-[120px]'>
-
-                    <ul className='space-y-6'>
-                        {
-                            walletData.map(item => <WalletItems key={item.id} item={item} />)
-                        }
-                    </ul>
-
-                    <ExploreBtn />
-                </div>
-
-            </div>
+          <ExploreBtn />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Wallet
+export default Wallet;
--- src/pages/Wallet/WalletItems.jsx
+++ src/pages/Wallet/WalletItems.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const WalletItems = ({ item }) => {
-    return (
-        <li className='flex items-center justify-between px-[100px] py-4 bg-lightBlue rounded-lg'>
-            <div className='flex items-center '>
-                <div><img src={item.img} alt="" /> </div>
-                <div className='ml-8 text-white w-[582.52px]'>
-                    <h2 className='font-syne font-bold text-2xl'>{item.name}</h2>
-                    <p className='font-manrope font-normal text-base opacity-[0.8]'>{item.desc}</p>
-                </div>
-            </div>
+  return (
+    <li className="flex flex-col lg:flex-row items-center justify-between lg:px-[100px] py-4 bg-lightBlue rounded-lg space-y-6 lg:space-y-0">
+      <div className="flex flex-col space-y-6 lg:space-y-0 lg:flex-row items-center">
+        <div>
+          <img src={item.img} alt="" />{" "}
+        </div>
+        <div className="lg:ml-8 text-white text-center lg:text-left max-w-[582.52px] space-y-3 lg:space-y-0 px-2 lg:px-0">
+          <h2 className="font-syne font-bold text-2xl">{item.name}</h2>
+          <p className="font-manrope font-normal text-base opacity-[0.8]">
+            {item.desc}
+          </p>
+        </div>
+      </div>
 
-            <div>
-                <button className='py-3 px-6 rounded-lg bg-darkBlue hover:bg-pink font-redHatDisplay font-medium text-white text-base'>Connect</button>
-            </div>
-        </li>
-    )
-}
+      <div>
+        <button className="py-3 px-6 rounded-lg bg-darkBlue hover:bg-pink font-redHatDisplay font-medium text-white text-base">
+          Connect
+        </button>
+      </div>
+    </li>
+  );
+};
 
-export default WalletItems
+export default WalletItems;
