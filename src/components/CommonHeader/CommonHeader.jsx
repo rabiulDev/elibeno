c3c0c7c80101b8e7e6d6454fae0b2aff4b90cedc
Rabiul Islam <rabiul.dev@gmail.com>
2022-08-13 11:50:21 +0600

    responsive added

--- src/components/CommonHeader/CommonHeader.jsx
+++ src/components/CommonHeader/CommonHeader.jsx
@@ -6,23 +6,26 @@ const CommonHeader = () => {
   const { state } = useLocation();
 
   return (
-    <header className="bg-bgNav bg-no-repeat bg-cover">
-      <div className="container mx-auto relative overflow-x-hidden">
+    <>
+      <header className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
-
-        <div className="mt-44 sm:mt-[147px] mb-6 sm:mb-[163px]">
-          <div className="flex flex-col sm:flex-row items-center sm:justify-between">
-            <h2 className="font-syne font-bold text-4xl md:text-[4rem] text-white">
-              {state}
-            </h2>
-            <p className="font-manrope font-semibold text-sm sm:text-base text-white opacity-[0.8]">
-              Home {">"}
-              <span className="text-pink"> {state} </span>
-            </p>
+      </header>
+      <section className="bg-bgNav bg-no-repeat bg-cover">
+        <div className="container mx-auto">
+          <div className="pt-44 sm:pt-[147px] pb-6 sm:pb-[163px]">
+            <div className="flex flex-col sm:flex-row items-center sm:justify-between">
+              <h2 className="font-syne font-bold text-4xl md:text-[4rem] text-white">
+                {state}
+              </h2>
+              <p className="font-manrope font-semibold text-sm sm:text-base text-white opacity-[0.8]">
+                Home {">"}
+                <span className="text-pink"> {state} </span>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </section>
+    </>
   );
 };
 
