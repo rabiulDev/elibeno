27c03edea601129d345e3be7855fdb6b8f852b83
Rabiul Islam <rabiul.dev@gmail.com>
2022-08-13 12:02:16 +0600

    responsive ranking table

--- src/pages/Ranking/RankItem.jsx
+++ src/pages/Ranking/RankItem.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const RankItem = ({ item }) => {
-    return (
-        <tr className='font-manrope font-normal text-base text-white'>
-            <td className='p-4 pr-8'>0{item.rank}</td>
-            <td className='p-4 pr-8 flex items-center'> <img className='mr-2' src={item.img} alt="" /> {item.name}</td>
-            <td className='p-4 pr-8'>{item.volume}</td>
-            <td className='p-4 pr-8 text-[#50FF24]'>{item.day}</td>
-            <td className='p-4 pr-8 text-[#FF4242]'>{item.week}</td>
-            <td className='p-4 pr-8'>{item.price}</td>
-            <td className='p-4 pr-8'>{item.owner}</td>
-            <td className='p-4 pr-8'>{item.asset}</td>
-        </tr>
-    )
-}
+  return (
+    <tr className="font-manrope font-normal text-base text-white">
+      <td className="p-4 pr-8">0{item.rank}</td>
+      <td className="p-4 pr-8 w-44 flex items-center">
+        <img className="mr-2" src={item.img} alt="" /> {item.name}
+      </td>
+      <td className="p-4 pr-8">{item.volume}</td>
+      <td className="p-4 pr-8 text-[#50FF24]">{item.day}</td>
+      <td className="p-4 pr-8 text-[#FF4242]">{item.week}</td>
+      <td className="p-4 pr-8">{item.price}</td>
+      <td className="p-4 pr-8">{item.owner}</td>
+      <td className="p-4 pr-8">{item.asset}</td>
+    </tr>
+  );
+};
 
-export default RankItem
+export default RankItem;
--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,38 +1,37 @@
-import rankings from '../../fakeDB/json/rank.json'
-import CommonHeader from '../../components/CommonHeader/CommonHeader'
-import ExploreBtn from '../../components/ExploreBtn/ExploreBtn'
-import RankItem from './RankItem'
+import rankings from "../../fakeDB/json/rank.json";
+import CommonHeader from "../../components/CommonHeader/CommonHeader";
+import ExploreBtn from "../../components/ExploreBtn/ExploreBtn";
+import RankItem from "./RankItem";
 
 const Ranking = () => {
-    return (
+  return (
+    <div>
+      <CommonHeader />
+      <div className="container mx-auto py-[120px] overflow-x-scroll xl:overflow-x-visible">
+        <table className="table-auto w-full rounded-lg border-collapse overflow-hidden">
+          <thead>
+            <tr className="bg-pink font-syne font-bold text-base text-white text-left">
+              <th className="p-4">Rank</th>
+              <th className="p-4">Collection</th>
+              <th className="p-4">Volume</th>
+              <th className="p-4">24h %</th>
+              <th className="p-4">7d %</th>
+              <th className="p-4">Floor Price</th>
+              <th className="p-4">Owners</th>
+              <th className="p-4">Assets</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rankings.map((item) => (
+              <RankItem key={item.rank} item={item} />
+            ))}
+          </tbody>
+        </table>
 
-        <div>
-            <CommonHeader />
-            <div className='container mx-auto py-[120px]'>
-                <table className='table-auto w-full rounded-lg border-collapse overflow-hidden'>
-                    <thead>
-                        <tr className='bg-pink font-syne font-bold text-base text-white text-left'>
-                            <th className='p-4'>Rank</th>
-                            <th className='p-4'>Collection</th>
-                            <th className='p-4'>Volume</th>
-                            <th className='p-4'>24h %</th>
-                            <th className='p-4'>7d %</th>
-                            <th className='p-4'>Floor Price</th>
-                            <th className='p-4'>Owners</th>
-                            <th className='p-4'>Assets</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            rankings.map(item => <RankItem key={item.rank} item={item} />)
-                        }
-                    </tbody>
-                </table>
+        <ExploreBtn />
+      </div>
+    </div>
+  );
+};
 
-                <ExploreBtn />
-            </div>
-        </div>
-    )
-}
-
-export default Ranking
+export default Ranking;
